ff6bf6724548ef9a0dc7d0cfa028eedc1c166cbb
hzhilong <1107495265@qq.com>
2025-09-16 21:02:11 +0800

    添加设置组、设置项布局组件； 添加主题颜色/模式更新的按钮组件；

---
+++ src/renderer/components/settings/UpdateThemeColorButton.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { useAppThemeStore } from '@/renderer/stores/app-theme'
+import { ThemeUtils } from '@/renderer/utils/theme-utils'
+import { AppConstants } from '@/shared/common/app-constants'
+import { ref } from 'vue'
+import CountdownDialog from '@/renderer/components/dialog/countdown/CountdownDialog.vue'
+
+// 保存的主题状态
+const appThemeStore = useAppThemeStore()
+const themeState = appThemeStore.state
+
+// 预设的颜色
+const predefineColors = ref(AppConstants.THEME.DEFAULT_PRIMARY_COLORS)
+// 新的主题色
+const selectedPrimaryColor = ref(themeState.primaryColor)
+// 旧的主题色
+let oldPrimaryColor = ''
+// 倒计时可见
+const countdownVisible = ref(false)
+
+// 更新主题色
+const updatePrimaryColor = () => {
+  if (!selectedPrimaryColor.value) return
+  // 尝试更新主题
+  oldPrimaryColor = themeState.primaryColor
+  ThemeUtils.updatePrimaryColor(selectedPrimaryColor.value)
+  // 倒计时提示是否保存
+  countdownVisible.value = true
+}
+const restoreTheme = () => {
+  selectedPrimaryColor.value = oldPrimaryColor
+  ThemeUtils.updatePrimaryColor(oldPrimaryColor)
+}
+</script>
+
+<template>
+  <el-color-picker v-model="selectedPrimaryColor" @change="updatePrimaryColor" :predefine="predefineColors" />
+  <CountdownDialog
+    v-model="countdownVisible"
+    content="是否保存当前主题？"
+    :countdown="10"
+    @onCancel="restoreTheme"
+  ></CountdownDialog>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/renderer/components/settings/UpdateThemeModeButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useAppThemeStore } from '@/renderer/stores/app-theme'
+import {ThemeUtils} from '@/renderer/utils/theme-utils'
+
+const state = useAppThemeStore().state
+
+const toggleTheme = () => {
+  ThemeUtils.toggleThemeMode(state.themeMode)
+}
+</script>
+
+<template>
+  <el-radio-group v-model="state.themeMode" @change="toggleTheme">
+    <el-radio-button label="浅色" value="light" />
+    <el-radio-button label="深色" value="dark" />
+    <el-radio-button label="系统" value="system" />
+  </el-radio-group>
+</template>
+
+<style scoped lang="scss"></style>
